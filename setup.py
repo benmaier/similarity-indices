e4a84f2c126a9b2395117f8dcdc0a29473d2bb12
Benjamin Maier <benjaminfrankmaier@gmail.com>
2016-06-25 14:30:23 +0200

    changed webpage in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='simind',
-      version='0.0.1',
+      version='0.0.2',
       description='Compute various incidence based or abundance based similarity indices between two numpy arrays.',
-      url='https://bitbucket.org/benmaier/similarity-indices',
+      url='https://github.com/benmaier/similarity-indices',
       author='Benjamin F. Maier',
       author_email='benjaminfrankmaier@gmail.com',
       license='MIT',
